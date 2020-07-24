693b99ff0d645437b9bde22a95f557fa9e957ec3
Max N <maximenorval@gmail.com>
2020-07-24 10:54:36 +0200

    [DONE] create components and services and add services to providers array in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book/book-list/book-list.component';
+import { SingleBookComponent } from './book/single-book/single-book.component';
+import { BookFormComponent } from './book/book-form/book-form.component';
+import { HeaderComponent } from './header/header.component';
+
+import { AuthService } from './services/auth.service';
+import { BookService } from './services/book.service';
+import { AuthGuardService } from './services/auth-guard.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    BookListComponent,
+    SingleBookComponent,
+    BookFormComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [AuthService, BookService, AuthGuardService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
